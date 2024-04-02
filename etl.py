3ac6ab965a80b88ac59b56cf4c50dcbe71da3445
workwithagung <107911369+workwithagung@users.noreply.github.com>
2024-04-02 09:31:07 +0700

    feat: add extract method

--- etl.py
+++ etl.py
@@ -1,2 +1,20 @@
+import pandas as pd
+
+
 class MarketingDataETL():
-    pass
+    
+    def __init__(self):
+        self.df = None
+
+    def extract(self, file_name, **kwargs):
+        """
+        Method ini menerima parameter file_name beserta opsi-opsi lain yang akan
+        dilemparkan ke fungsi read_csv
+
+        parameters:
+        - file_name : str : nama file yang akan dibaca
+        - **kwargs : any : opsi yang akan dilempar ke method read_csv pandas
+        """
+        self.df = pd.read_csv(file_name, **kwargs)
+
+        return self
